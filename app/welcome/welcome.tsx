1a2e57d6056992b62a26fbb33aa6dca60de7466b
max <max.ruchkin@gmail.com>
2024-12-14 15:17:52 +0200

    feat: shadcn

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -1,10 +1,11 @@
 import logoDark from "./logo-dark.svg";
 import logoLight from "./logo-light.svg";
 import {NavLink} from "react-router";
+import {Button} from "~/components/ui/button"
 
 export function Welcome() {
   return (
-    <main className="flex items-center justify-center pt-16 pb-4">
+    <main className="flex items-center justify-center pt-16 pb-4 mb-1">
       <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
         <header className="flex flex-col items-center gap-9">
           <div className="w-[500px] max-w-[100vw] p-4">
@@ -21,7 +22,8 @@ export function Welcome() {
           </div>
         </header>
         <div className="max-w-[300px] w-full space-y-6 px-4">
-          <NavLink to="/about" >
+          <Button>Button</Button>
+          <NavLink to="/about" className='font-roboto'>
             About
           </NavLink>
           <nav className="rounded-3xl border border-gray-200 p-6 dark:border-gray-700 space-y-4">
@@ -29,7 +31,7 @@ export function Welcome() {
               What&apos;s next?
             </p>
             <ul>
-              {resources.map(({ href, text, icon }) => (
+              {resources.map(({href, text, icon}) => (
                 <li key={href}>
                   <a
                     className="group flex items-center gap-3 self-stretch p-3 leading-normal text-blue-700 hover:underline dark:text-blue-500"
