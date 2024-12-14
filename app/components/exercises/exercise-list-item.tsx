a4eeb12366c89ead690c4deff4a8c3ad10697402
max <max.ruchkin@gmail.com>
2024-12-14 20:01:11 +0200

    feat: exercise list item dynamic checkbox

--- app/components/exercises/exercise-list-item.tsx
+++ app/components/exercises/exercise-list-item.tsx
@@ -10,19 +10,29 @@ import {
 } from "~/components/ui/dropdown-menu"
 import {Separator} from "@radix-ui/react-separator";
 import type {Exercise} from "~/types";
-import {Trash2, Edit, EllipsisVerticalIcon, History} from "lucide-react";
+import {Edit, EllipsisVerticalIcon, History, Trash2} from "lucide-react";
 import {Button} from "~/components/ui/button";
 
+import {Checkbox} from "~/components/ui/checkbox"
 
-export function ExerciseListItem({name, id}: Exercise) {
 
-
+export function ExerciseListItem({name, id, hasCheckbox}: Exercise & { hasCheckbox?: boolean; }) {
   return (
     <li>
-      <div className='flex justify-between w-full'>
-        <span className='text-base'>{name}</span>
+      <div className='flex justify-between'>
+        {!hasCheckbox ?
+          <div className="flex items-center space-x-2 grow">
+            <Checkbox id={id}/>
+            <label
+              htmlFor={id}
+              className="text-base h-full w-full flex items-center cursor-pointer peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+            >
+              {name}
+            </label>
+          </div> :
+          <span>{name}</span>}
         <DropdownMenu>
-          <DropdownMenuTrigger>
+          <DropdownMenuTrigger asChild>
             <Button variant='ghost'>
               <EllipsisVerticalIcon/>
             </Button>
