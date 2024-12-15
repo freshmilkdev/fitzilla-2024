dd01dbefa6b6c0ac8c01e606c3e64ea688c03396
max <max.ruchkin@gmail.com>
2024-12-15 14:21:05 +0200

    feat: base top navbar

---
+++ app/components/layout/top-nav-bar.tsx
@@ -0,0 +1,6 @@
+export default function TopNavBar() {
+  return (
+    <nav className="bg-white h-16 border-b border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+
+  </nav>)
+}
--- app/layout/layout.tsx
+++ app/layout/layout.tsx
@@ -1,10 +1,14 @@
 import {Outlet} from "react-router";
 import {BottomNavigation} from "~/components/layout/bottom-navigation";
+import TopNavBar from "~/components/layout/top-nav-bar";
 
 export default function Layout() {
   return (
     <main className="flex flex-col justify-between min-h-screen">
-      <Outlet/>
+      <div className='flex flex-col'>
+        <TopNavBar/>
+        <Outlet/>
+      </div>
       <BottomNavigation/>
     </main>
   )
