721156a267c7047387f5de4991f045b24d50241c
max <max.ruchkin@gmail.com>
2024-12-20 15:58:07 +0200

    feat: workout exercise list

--- app/components/workout/workout-exercise-list.tsx
+++ app/components/workout/workout-exercise-list.tsx
@@ -1,73 +1,36 @@
-import type {Exercise} from "~/types";
-import {Separator} from "~/components/ui/separator";
-import {NavLink} from "react-router";
-import {ChevronRight} from "lucide-react";
-import {Badge} from "~/components/ui/badge";
-import {routePaths} from "~/routes";
+import { useWorkout } from "~/context/workout-context";
+import { Separator } from "../ui/separator";
+import { ChevronRight } from "lucide-react";
+import { Badge } from "../ui/badge";
+import { NavLink } from "react-router";
+import { routePaths } from "~/routes";
 
-const exercises: Exercise[] = [
-  {
-    id: 1, 
-    name: 'Pull Up',
-    muscleGroupId: 1,
-    isBodyweight: true,
-    createdAt: new Date(),
-    updatedAt: new Date()
-  },
-  {
-    id: 2, 
-    name: 'Lat Pulldown',
-    muscleGroupId: 1,
-    isBodyweight: true,
-    createdAt: new Date(),
-    updatedAt: new Date()
-  },
-  {
-    id: 3, 
-    name: 'Seated Row',
-    muscleGroupId: 1,
-    isBodyweight: true,
-    createdAt: new Date(),
-    updatedAt: new Date()
-  },
-  {
-    id: 4, 
-    name: 'Bent Over Row',
-    muscleGroupId: 1,
-    isBodyweight: true,
-    createdAt: new Date(),
-    updatedAt: new Date()
-  },
-  {
-    id: 5, 
-    name: 'Deadlift',
-    muscleGroupId: 1,
-    isBodyweight: true,
-    createdAt: new Date(),
-    updatedAt: new Date()
-  },
-  {
-    id: 6, 
-    name: 'Squat',
-    muscleGroupId: 1,
-    isBodyweight: true,
-    createdAt: new Date(),
-    updatedAt: new Date()
-  },
-]
 export default function WorkoutExerciseList() {
+  const { workoutExercises } = useWorkout();
+
+  if (!workoutExercises.length) {
+    return <div className="p-4 text-center text-muted-foreground">No exercises added yet.</div>;
+  }
+
   return (
     <ul>
-      {exercises.map(({id, name}: Exercise) =>
-        <li key={`exercise-${id}`} className='hover:bg-slate-100 dark:hover:bg-slate-950 transition duration-200'>
-          <NavLink to={`${routePaths.exercises}/${id}`} className='flex items-center pr-2 py-3'>
+      {workoutExercises.map((exercise) => (
+        <li key={exercise.id} className='hover:bg-slate-100 dark:hover:bg-slate-950 transition duration-200'>
+          <NavLink 
+            to={`${routePaths.workout}/${exercise.exerciseId}`} 
+            className='flex items-center px-4 py-3'
+          >
             <span className='flex grow'>
-              <span className='ml-2'>{name}</span>
-              </span>
-            <Badge variant='secondary'>3</Badge>
-            <ChevronRight size={18}/>
+              <span>{exercise.exerciseName}</span>
+            </span>
+            <Badge variant='secondary' className="mr-2">
+              {exercise.sets.length}
+            </Badge>
+            <ChevronRight size={18} className="text-muted-foreground"/>
           </NavLink>
-          <Separator/>
-        </li>)}
-    </ul>)
+          <Separator />
+        </li>
+      ))}
+    </ul>
+  );
 }
